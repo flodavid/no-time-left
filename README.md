273112faa8b04d358d2ee1cfe542a76195260e24
flodavid <fl.david.53@gmail.com>
2024-08-12 14:15:15 +0200

    Save words to a query parameter

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # no-time-left
+
+## Run
+
+```
+npm install
+npm start
+```
+
+## Build
+
+```
+npm run build
+```
+
+To test after building, use `npm run preview`
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupRandomWord } from './words.ts'
+import { setupNextWord, setupGuessedWord } from './words.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    <h1>Random word</h1>
-    <div class="card">
-      <button id="random_word" type="button">START</button>
-      <p id="word"></p>
-    </div>
-  </div>
-`
 
-setupRandomWord(document.querySelector<HTMLButtonElement>('#random_word')!, document.querySelector<HTMLParagraphElement>('#word')!)
+setupNextWord(document.querySelector<HTMLButtonElement>('#next_word')!,
+              document.querySelector<HTMLButtonElement>('#guessed_word')!,
+              document.querySelector<HTMLParagraphElement>('#word')!)
+setupGuessedWord(document.querySelector<HTMLButtonElement>('#guessed_word')!,
+                  document.querySelector<HTMLParagraphElement>('#word')!)
